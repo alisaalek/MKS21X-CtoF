15e6ccdba60f0a1f01caadf4d9d0995d75112fb9
Alisa Aleksandrova <aaleksandrova30@stuy.edu>
2021-09-23 22:08:10 -0400

    Add main method

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,11 +1,16 @@
 // The functions should have doubles as parameters. They should return doubles as well.
 
 public class CtoFTester {
-    public double celsiusToFahrenheit(double celsius) {
+    public static double celsiusToFahrenheit(double celsius) {
         return celsius * 1.8 + 32;
     }
 
-    public double fahrenheitToCelsius(double fahrenheit) {
+    public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) / 1.8;
+    }
+
+    public static void main(String[] argv) {
+        System.out.println(celsiusToFahrenheit(100));
+        System.out.println(fahrenheitToCelsius(100));
     }
 }
